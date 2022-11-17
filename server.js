1002199aba174119ced3ed0c147d2ba655324235
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2022-11-17 14:29:08 -0300

    DB connect

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+/* 
+    Requires
+*/
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
@@ -5,8 +8,38 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const app = express()
 
-app
-  .get('/', (req, res) => {
-    res.send('My app test!')
+/* 
+    Database connected - emit
+*/
+mongoose
+  .connect(process.env.DB_CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .listen(3000)
+  .then(() => {
+    app.emit('success')
+  })
+  .catch(e => console.log(e))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ Message: 'My app test!' })
+})
+
+/* 
+    JSON config
+*/
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+app.use(express.json())
+
+/* 
+    Database connected - on
+*/
+app.on('success', () => {
+  app.listen(3000, () => {
+    console.log('Server running in http://localhost:3000')
+  })
+})
